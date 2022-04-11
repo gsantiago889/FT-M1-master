1db262054be7f9c38e15460e9d99186f01404151
gsantiago889 <gsantiago889@hotmail.com>
2022-04-11 19:05:28 -0300

    homework1

--- 00-IntroToCS/homework/homework.js
+++ 00-IntroToCS/homework/homework.js
@@ -2,11 +2,33 @@
 
 function BinarioADecimal(num) {
   // tu codigo aca
+  num = num+'';
+  let bin = num;
+  num = num.toString().split('').reverse();
+  let resultado=0;
+  
+  for(let i = 0; i < num.length; i ++){      
+      resultado = resultado + (num[i] * Math.pow(2, i));       
+    }
+
+  console.log('Binario ingresado: '+ bin + ' resultado en decimal : ' + resultado);  
+  return resultado;
 
 }
 
 function DecimalABinario(num) {
   // tu codigo aca
+  let dec = num;
+  let i=0
+  let bin=[];
+  while((num / 2) > 0){
+      bin.push(num % 2);
+      num = Math.floor(num /= 2);
+  }
+  let resultado = bin.reverse();
+  resultado = resultado.join('');
+  console.log('Decimal ingresado: '+ dec + ' resultado en binario: '+ resultado);  
+  return resultado;
 
 }
 
